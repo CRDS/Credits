4d558626a9cf0754b646609fb7f47f17c44051b7
Peter Bushnell <bushsolo@gmail.com>
2017-09-20 14:43:29 +0100

    Dynamic git info

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -48,8 +48,8 @@ const std::string CLIENT_NAME("Credits");
 
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "7b42942"
-#define GIT_COMMIT_DATE "Tue, 14 Feb 2017 10:59:58 +0100"
+#define GIT_COMMIT_ID "$Format:%h$"
+#define GIT_COMMIT_DATE "$Format:%cD$"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
