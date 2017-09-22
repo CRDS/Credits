3d08828b82bce8286200850947425386723d6acb
Peter Bushnell <bushsolo@gmail.com>
2017-09-22 14:40:41 +0100

    Add brackets to suppress warning

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2412,10 +2412,13 @@ void CWallet::AvailableCoins(std::vector<COutput>& vCoins, bool fOnlyConfirmed, 
                 } else if(nCoinType == ONLY_NONDENOMINATED_NOT500IFMN) {
                     if (IsCollateralAmount(pcoin->vout[i].nValue)) continue; // do not use collateral amounts
                     found = !IsDenominatedAmount(pcoin->vout[i].nValue);
-                    if (chainActive.Height() < Params().GetConsensus().nHardForkOne)
-                        if(found && fMasterNode) found = pcoin->vout[i].nValue != 500*COIN; // do not use Hot MN funds
-                    else
-                        if(found && fMasterNode) found = pcoin->vout[i].nValue != 5000*COIN; // do not use Hot MN funds
+                    if (chainActive.Height() < Params().GetConsensus().nHardForkOne) {
+                        if(found && fMasterNode)
+                            found = pcoin->vout[i].nValue != 500*COIN; // do not use Hot MN funds
+                    } else {
+                        if(found && fMasterNode)
+                            found = pcoin->vout[i].nValue != 5000*COIN; // do not use Hot MN funds
+                    }
                 } else if(nCoinType == ONLY_500) {
                     if (chainActive.Height() < Params().GetConsensus().nHardForkOne)
                         found = pcoin->vout[i].nValue == 500*COIN;
