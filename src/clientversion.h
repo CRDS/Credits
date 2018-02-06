c34423935c79668afcdae1b8478b8995be3da422
Andrew Tookay <andrewtookay@users.noreply.github.com>
2018-02-06 17:23:09 +0200

    Updated clientversion.h to v1.2

--- src/clientversion.h
+++ src/clientversion.h
@@ -19,14 +19,14 @@
 
 //! These need to be macros, as clientversion.cpp's and credits*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 1
-#define CLIENT_VERSION_MINOR 1
-#define CLIENT_VERSION_REVISION 1
+#define CLIENT_VERSION_MINOR 2
+#define CLIENT_VERSION_REVISION 0
 #define CLIENT_VERSION_BUILD 0
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
 
-#define COPYRIGHT_YEAR 2017
+#define COPYRIGHT_YEAR 2018
 
 #endif //HAVE_CONFIG_H
 
@@ -38,7 +38,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) " Credits Developers, "
+#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2017-" STRINGIZE(COPYRIGHT_YEAR) " Credits Developers, "
 
 /**
  * creditsd-res.rc includes this file, but it cannot cope with real c++ code.
