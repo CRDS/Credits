4d9ce75115e673ceebac773dd5ca4f42cb92c3dc
Andrew Tookay <andrewtookay@users.noreply.github.com>
2019-05-16 22:40:33 +0300

    Update argon2.c

--- src/crypto/argon2d/argon2.c
+++ src/crypto/argon2d/argon2.c
@@ -4,7 +4,7 @@
  * Copyright 2015
  * Daniel Dinu, Dmitry Khovratovich, Jean-Philippe Aumasson, and Samuel Neves
  *
- * You may use this work under the terms of a Creative Commons CC0 1.0 
+ * You may use this work under the terms of a Creative Commons CC0 1.0
  * License/Waiver or the Apache Public License 2.0, at your option. The terms of
  * these licenses can be found at:
  *
@@ -393,7 +393,7 @@ size_t argon2_encodedlen(uint32_t t_cost, uint32_t m_cost, uint32_t parallelism,
 #include <x86intrin.h>
 #include "../blake2/blake2.h"
 
-typedef struct _Argon2d_Block
+ typedef struct _Argon2d_Block
 {
 	union
 	{
@@ -403,18 +403,18 @@ typedef struct _Argon2d_Block
 	};
 } Argon2d_Block;
 
-typedef struct _Argon2ThreadData
+ typedef struct _Argon2ThreadData
 {
 	Argon2d_Block *Matrix;
 	uint32_t slice;
 	uint32_t lane;
 } Argon2ThreadData;
 
-#define SEGMENT_LENGTH			(250U / (4U * 4U))		// memory_blocks / (context->lanes * ARGON2_SYNC_POINTS);
+ #define SEGMENT_LENGTH			(250U / (4U * 4U))		// memory_blocks / (context->lanes * ARGON2_SYNC_POINTS);
 #define LANE_LENGTH				(SEGMENT_LENGTH * 4U)	// segment_length * ARGON2_SYNC_POINTS;
 #define CONCURRENT_THREADS		4
 
-static const uint64_t blake2b_IV[8] =
+ static const uint64_t blake2b_IV[8] =
 {
 	0x6A09E667F3BCC908ULL, 0xBB67AE8584CAA73BULL,
 	0x3C6EF372FE94F82BULL, 0xA54FF53A5F1D36F1ULL,
@@ -422,7 +422,7 @@ static const uint64_t blake2b_IV[8] =
 	0x1F83D9ABFB41BD6BULL, 0x5BE0CD19137E2179ULL
 };
 
-static const unsigned int blake2b_sigma[12][16] =
+ static const unsigned int blake2b_sigma[12][16] =
 {
 	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
 	{14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
@@ -438,9 +438,9 @@ static const unsigned int blake2b_sigma[12][16] =
 	{14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
 };
 
-#define ROTL64(x, y)		(((x) << (y)) | ((x) >> (64 - (y))))
+ #define ROTL64(x, y)		(((x) << (y)) | ((x) >> (64 - (y))))
 
-#define G(r, i, a, b, c, d)                                                    \
+ #define G(r, i, a, b, c, d)                                                    \
     do {                                                                       \
         a = a + b + m[blake2b_sigma[r][2 * i + 0]];                            \
         d = ROTL64(d ^ a, 32);                                                 \
@@ -452,7 +452,7 @@ static const unsigned int blake2b_sigma[12][16] =
         b = ROTL64(b ^ c, 1);                                                 \
     } while ((void)0, 0)
 
-#define ROUND(r)                                                               \
+ #define ROUND(r)                                                               \
     do {                                                                       \
         G(r, 0, v[0], v[4], v[8], v[12]);                                      \
         G(r, 1, v[1], v[5], v[9], v[13]);                                      \
@@ -464,70 +464,66 @@ static const unsigned int blake2b_sigma[12][16] =
         G(r, 7, v[3], v[4], v[9], v[14]);                                      \
     } while ((void)0, 0)
 
-void CompressBlock(uint64_t *h, const uint64_t *m, uint64_t t, uint64_t f)
+ void CompressBlock(uint64_t *h, const uint64_t *m, uint64_t t, uint64_t f)
 {
 	uint64_t v[16];
-	
-	int i;
-	for(i = 0; i < 8; ++i) v[i] = h[i];
-	
-	for(i = 8; i < 16; ++i) v[i] = blake2b_IV[i - 8];
-	
-	v[12] ^= t;
+
+ 	for(int i = 0; i < 8; ++i) v[i] = h[i];
+
+ 	for(int i = 8; i < 16; ++i) v[i] = blake2b_IV[i - 8];
+
+ 	v[12] ^= t;
 	v[14] ^= f;
-	
-	int r;
-	for(r = 0; r < 12; ++r)
+
+ 	for(int r = 0; r < 12; ++r)
 	{
 		ROUND(r);
 	}
-	
-	for(i = 0; i < 8; ++i) h[i] ^= v[i] ^ v[i + 8];
+
+ 	for(int i = 0; i < 8; ++i) h[i] ^= v[i] ^ v[i + 8];
 }
 
-void Argon2dInitHash(void *HashOut, void *Input)
+ void Argon2dInitHash(void *HashOut, void *Input)
 {
 	blake2b_state BlakeHash;
 	uint32_t InBuf[64];							// Is only 50 uint32_t, but need more space for Blake2B
-	
-	memset(InBuf, 0x00, 200);
-	
-	InBuf[0] = 4UL;								// Lanes
+
+ 	memset(InBuf, 0x00, 200);
+
+ 	InBuf[0] = 4UL;								// Lanes
 	InBuf[1] = 32UL;								// Output Length
 	InBuf[2] = 250UL;							// Memory Cost
 	InBuf[3] = 1UL;								// Time Cost
 	InBuf[4] = 16UL;								// Argon2 Version Number
 	InBuf[5] = 0UL;								// Type
 	InBuf[6] = 80UL;								// Password Length
-	
-	memcpy(InBuf + 7, Input, 80);				// Password
-	
-	InBuf[27] = 80UL;							// Salt Length
-	
-	memcpy(InBuf + 28, Input, 80);				// Salt
-	
-	InBuf[48] = 0UL;								// Secret Length
+
+ 	memcpy(InBuf + 7, Input, 80);				// Password
+
+ 	InBuf[27] = 80UL;							// Salt Length
+
+ 	memcpy(InBuf + 28, Input, 80);				// Salt
+
+ 	InBuf[48] = 0UL;								// Secret Length
 	InBuf[49] = 0UL;								// Associated Data Length
-	
-	int i;
-	for(i = 50; i < 64; ++i) InBuf[i] = 0UL;
-		
-	uint64_t H[8];
-	
-	for(i = 0; i < 8; ++i) H[i] = blake2b_IV[i];
-	
-	H[0] ^= 0x0000000001010040;
-	
-	CompressBlock(H, (uint64_t *)InBuf, 128ULL, 0ULL);
+
+ 	for(int i = 50; i < 64; ++i) InBuf[i] = 0UL;
+
+ 	uint64_t H[8];
+
+ 	for(int i = 0; i < 8; ++i) H[i] = blake2b_IV[i];
+
+ 	H[0] ^= 0x0000000001010040;
+
+ 	CompressBlock(H, (uint64_t *)InBuf, 128ULL, 0ULL);
 	CompressBlock(H, (uint64_t *)(InBuf + 32), 200ULL, 0xFFFFFFFFFFFFFFFFULL);
-	
-	memcpy(HashOut, H, 64U);
+
+ 	memcpy(HashOut, H, 64U);
 }
 
-void Argon2dFillFirstBlocks(Argon2d_Block *Matrix, void *InitHash)
+ void Argon2dFillFirstBlocks(Argon2d_Block *Matrix, void *InitHash)
 {
-	uint32_t lane;
-	for(lane = 0; lane < 4; ++lane)
+	for(uint32_t lane = 0; lane < 4; ++lane)
 	{
 		((uint32_t *)InitHash)[16] = 0;
 		((uint32_t *)InitHash)[17] = lane;
@@ -537,134 +533,170 @@ void Argon2dFillFirstBlocks(Argon2d_Block *Matrix, void *InitHash)
 	}
 }
 
-#include "../blake2/blamka-round-opt.h"
+ #include "../blake2/blamka-round-opt.h"
 
-void Argon2dFillSingleBlock(Argon2d_Block *State, Argon2d_Block *RefBlock, Argon2d_Block *NextBlock)
-{	
-	__m256i XY[32];
-	
-	int i;
-	for(i = 0; i < 32; ++i)
+ void Argon2dFillSingleBlock(Argon2d_Block *State, Argon2d_Block *RefBlock, Argon2d_Block *NextBlock)
+{
+	/*#if 0
+	//__m128i XY[64];
+
+ 	//for(int i = 0; i < 64; ++i)
+	//	XY[i] = NextBlock->dqwords[i] = _mm_xor_si128(State->dqwords[i], RefBlock->dqwords[i]);
+
+ 	__m256i XY[32];
+
+ 	for(int i = 0; i < 32; ++i) XY[i] = NextBlock->qqwords[i] = _mm256_xor_si256(State->qqwords[i], RefBlock->qqwords[i]);
+
+ 	for(int i = 0; i < 8; ++i)
+	{
+		BLAKE2_ROUND(	NextBlock->dqwords[8 * i + 0], NextBlock->dqwords[8 * i + 1], NextBlock->dqwords[8 * i + 2], NextBlock->dqwords[8 * i + 3],
+						NextBlock->dqwords[8 * i + 4], NextBlock->dqwords[8 * i + 5], NextBlock->dqwords[8 * i + 6], NextBlock->dqwords[8 * i + 7]);
+	}
+
+ 	for(int i = 0; i < 8; ++i)
+	{
+		BLAKE2_ROUND(	NextBlock->dqwords[8 * 0 + i], NextBlock->dqwords[8 * 1 + i], NextBlock->dqwords[8 * 2 + i], NextBlock->dqwords[8 * 3 + i],
+						NextBlock->dqwords[8 * 4 + i], NextBlock->dqwords[8 * 5 + i], NextBlock->dqwords[8 * 6 + i], NextBlock->dqwords[8 * 7 + i]);
+	}
+
+ 	//for(int i = 0; i < 64; ++i)
+	//{
+	//	NextBlock->dqwords[i] ^= XY[i];
+	//}
+
+ 	for(int i = 0; i < 32; ++i) NextBlock->qqwords[i] = _mm256_xor_si256(NextBlock->qqwords[i], XY[i]);
+
+ 	#else*/
+
+ 	__m256i XY[32];
+
+ 	for(int i = 0; i < 32; ++i)
 		XY[i] = State->qqwords[i] = _mm256_xor_si256(State->qqwords[i], RefBlock->qqwords[i]);
-	
-	for(i = 0; i < 8; ++i)
+
+ 	for(int i = 0; i < 8; ++i)
 	{
 		BLAKE2_ROUND(	State->dqwords[8 * i + 0], State->dqwords[8 * i + 1], State->dqwords[8 * i + 2], State->dqwords[8 * i + 3],
 						State->dqwords[8 * i + 4], State->dqwords[8 * i + 5], State->dqwords[8 * i + 6], State->dqwords[8 * i + 7]);
 	}
-	
-	for(i = 0; i < 8; ++i)
+
+ 	for(int i = 0; i < 8; ++i)
 	{
 		BLAKE2_ROUND(	State->dqwords[8 * 0 + i], State->dqwords[8 * 1 + i], State->dqwords[8 * 2 + i], State->dqwords[8 * 3 + i],
 						State->dqwords[8 * 4 + i], State->dqwords[8 * 5 + i], State->dqwords[8 * 6 + i], State->dqwords[8 * 7 + i]);
 	}
-	
-	for(i = 0; i < 32; ++i)
+
+ 	for(int i = 0; i < 32; ++i)
 	{
 		State->qqwords[i] = _mm256_xor_si256(State->qqwords[i], XY[i]);
 		_mm256_store_si256(NextBlock->qqwords + i, State->qqwords[i]);
 	}
 }
 
-void FillSegment(Argon2d_Block *Matrix, uint32_t slice, uint32_t lane)
-{			
+ void FillSegment(Argon2d_Block *Matrix, uint32_t slice, uint32_t lane)
+{
 	uint32_t startidx, prevoff, curoff;
 	Argon2d_Block State;
-	
-	startidx = (!slice) ? 2 : 0;
+
+ 	startidx = (!slice) ? 2 : 0;
 	curoff = lane * LANE_LENGTH + slice * SEGMENT_LENGTH + startidx;
-	
-	//if(!(curoff % LANE_LENGTH)) prevoff = curoff + LANE_LENGTH - 1;
+
+ 	//if(!(curoff % LANE_LENGTH)) prevoff = curoff + LANE_LENGTH - 1;
 	//else prevoff = curoff - 1;
-	
-	prevoff = (!(curoff % LANE_LENGTH)) ? curoff + LANE_LENGTH - 1 : curoff - 1;
-	
-	memcpy(State.data, (Matrix + prevoff)->data, 1024);
-	
-	int i;
-	for(i = startidx; i < SEGMENT_LENGTH; ++i, ++curoff, ++prevoff)
+
+ 	prevoff = (!(curoff % LANE_LENGTH)) ? curoff + LANE_LENGTH - 1 : curoff - 1;
+
+ 	memcpy(State.data, (Matrix + prevoff)->data, 1024);
+
+ 	for(int i = startidx; i < SEGMENT_LENGTH; ++i, ++curoff, ++prevoff)
 	{
 		if((curoff % LANE_LENGTH) == 1) prevoff = curoff - 1;
-		
-		uint64_t pseudorand = Matrix[prevoff].data[0];
+
+ 		uint64_t pseudorand = Matrix[prevoff].data[0];
 		uint64_t reflane = (!slice) ? lane : (pseudorand >> 32) & 3;		// mod lanes
-				
-		uint32_t index = i;
+
+ 		uint32_t index = i;
 		bool samelane = reflane == lane;
 		pseudorand &= 0xFFFFFFFFULL;
 		uint32_t refareasize = ((reflane == lane) ? slice * SEGMENT_LENGTH + index - 1 : slice * SEGMENT_LENGTH + ((!index) ? -1 : 0));
-		
-		
-		if(!slice) refareasize = index - 1;
-		
-		uint64_t relativepos = (pseudorand & 0xFFFFFFFFULL);
+
+
+ 		if(!slice) refareasize = index - 1;
+
+ 		uint64_t relativepos = (pseudorand & 0xFFFFFFFFULL);
 		relativepos = relativepos * relativepos >> 32;
 		relativepos = refareasize - 1 - (refareasize * relativepos >> 32);
-		
-		uint32_t startpos = 0;
-				
-		uint32_t abspos = (startpos + relativepos) % LANE_LENGTH;
-		
-		uint32_t refidx = abspos;
-		
-		Argon2dFillSingleBlock(&State, Matrix + (LANE_LENGTH * reflane + refidx), Matrix + curoff);
+
+ 		uint32_t startpos = 0;
+
+ 		uint32_t abspos = (startpos + relativepos) % LANE_LENGTH;
+
+ 		uint32_t refidx = abspos;
+
+ 		Argon2dFillSingleBlock(&State, Matrix + (LANE_LENGTH * reflane + refidx), Matrix + curoff);
 	}
 }
 
-void *ThreadedSegmentFill(void *ThrData)
+ void *ThreadedSegmentFill(void *ThrData)
 {
 	Argon2ThreadData *Data = (Argon2ThreadData *)ThrData;
-	
-	FillSegment(Data->Matrix, Data->slice, Data->lane);
+
+ 	FillSegment(Data->Matrix, Data->slice, Data->lane);
 	return(NULL);
 }
 
-void Argon2dFillAllBlocks(Argon2d_Block *Matrix)
+ void Argon2dFillAllBlocks(Argon2d_Block *Matrix)
 {
 	pthread_t ThrHandles[CONCURRENT_THREADS];
 	Argon2ThreadData ThrData[CONCURRENT_THREADS];
-	
-	int s;
-	for(s = 0; s < 4; ++s)
+
+ 	for(int s = 0; s < 4; ++s)
 	{
 		// WARNING: Assumes CONCURRENT_THREADS == lanes == 4
-		int l;
-		for(l = 0; l < 4; ++l)
+		for(int l = 0; l < 4; ++l)
 		{
+			/*
+			ThrData[l].Matrix = Matrix;
+			ThrData[l].slice = s;
+			ThrData[l].lane = l;
+
+			pthread_create(ThrHandles + l, NULL, ThreadedSegmentFill, (void *)(ThrData + l));
+			*/
 			FillSegment(Matrix, s, l);
-		}		
+		}
+
+ 		//for(int i = 0; i < CONCURRENT_THREADS; ++i) pthread_join(ThrHandles[i], NULL);
 	}
 }
 
-void Argon2dFinalizeHash(void *OutputHash, Argon2d_Block *Matrix)
+ void Argon2dFinalizeHash(void *OutputHash, Argon2d_Block *Matrix)
 {
-	int l;
-	for(l = 1; l < 4; ++l)
+	for(int l = 1; l < 4; ++l)
 	{
-		int i;
-		for(i = 0; i < 32; ++i)
+		for(int i = 0; i < 32; ++i)
 			Matrix[LANE_LENGTH - 1].qqwords[i] = _mm256_xor_si256(Matrix[LANE_LENGTH - 1].qqwords[i], Matrix[LANE_LENGTH * l + (LANE_LENGTH - 1)].qqwords[i]);
 	}
-	
-	blake2b_long(OutputHash, 32, Matrix[LANE_LENGTH - 1].data, 1024);
+
+ 	blake2b_long(OutputHash, 32, Matrix[LANE_LENGTH - 1].data, 1024);
 }
 
-void WolfArgon2dPoWHash(void *Output, void *Matrix, const void *BlkHdr)
+
+
+ void WolfArgon2dPoWHash(void *Output, void *Matrix, const void *BlkHdr)
 {
 	uint8_t tmp[72];
-		
-	Argon2dInitHash(tmp, (uint8_t *)BlkHdr);
-		
-	Argon2dFillFirstBlocks(Matrix, tmp);
-	
-	Argon2dFillAllBlocks(Matrix);
-	
-	Argon2dFinalizeHash((uint8_t *)Output, Matrix);
+
+ 	Argon2dInitHash(tmp, (uint8_t *)BlkHdr);
+
+ 	Argon2dFillFirstBlocks(Matrix, tmp);
+
+ 	Argon2dFillAllBlocks(Matrix);
+
+ 	Argon2dFinalizeHash((uint8_t *)Output, Matrix);
 }
 
-void WolfArgon2dAllocateCtx(void **Matrix)
+ void WolfArgon2dAllocateCtx(void **Matrix)
 {
-	#ifdef _WIN32
+  #ifdef _WIN32
 	*((Argon2d_Block **)Matrix) = (Argon2d_Block *)_aligned_malloc(32, sizeof(Argon2d_Block) * (SEGMENT_LENGTH << 4));
 	#else
 	*((Argon2d_Block **)Matrix) = (Argon2d_Block *)malloc(sizeof(Argon2d_Block) * (SEGMENT_LENGTH << 4));
@@ -672,7 +704,7 @@ void WolfArgon2dAllocateCtx(void **Matrix)
 	#endif
 }
 
-void WolfArgon2dFreeCtx(void *Matrix)
+ void WolfArgon2dFreeCtx(void *Matrix)
 {
 	free(Matrix);
 }
