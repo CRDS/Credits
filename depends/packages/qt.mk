45a22ee7564566185b56e32f353d7f6ded931684
razerrazer <dazzpirate@gmail.com>
2019-08-21 09:14:24 +0200

    Update qt.mk

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -141,7 +141,7 @@ define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/fix-xcb-include-order.patch && \
   patch -p1 < $($(package)_patch_dir)/fix_qt_pkgconfig.patch && \
   patch -p1 < $($(package)_patch_dir)/fix_qfontengine_coretext.patch && \
-  patch -p1 < $($(package)_patch_dir)/fix_qcocoahelpers.patch && \  
+  patch -p1 < $($(package)_patch_dir)/fix_qcocoahelpers.patch && \
   echo "!host_build: QMAKE_CFLAGS     += $($(package)_cflags) $($(package)_cppflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
   echo "!host_build: QMAKE_CXXFLAGS   += $($(package)_cxxflags) $($(package)_cppflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
   echo "!host_build: QMAKE_LFLAGS     += $($(package)_ldflags)" >> qtbase/mkspecs/common/gcc-base.conf && \
